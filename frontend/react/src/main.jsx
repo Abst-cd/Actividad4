4a9921120a93dfce656e13f52a37454d2822f5a0
Abstract <pacodejesus2006@gmail.com>
2026-02-15 19:50:05 -0600

    changes

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -6,17 +6,14 @@ import Login from './Components/Login.jsx'
 import './Components/Login.css';
 
 function Log() {
-  // --- PASO 1: Estado para saber si el usuario está logueado ---
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <>
       {loggedIn ? (
-        // --- PASO 2: Si está logueado, muestra el Recetario ---
         <App />
       ) : (
-        // --- PASO 3: Si NO está logueado, muestra el Login ---
-        // Pasamos la función para cambiar el estado cuando sea exitoso
+     
         <Login onLoginSuccess={() => setLoggedIn(true)} />
       )}
     </>
@@ -25,7 +22,7 @@ function Log() {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* --- PASO 4: Renderiza el gestor Log --- */}
+    
     <Log />
   </StrictMode>,
 )
